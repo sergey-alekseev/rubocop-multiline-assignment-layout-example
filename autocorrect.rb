ef36ce02a0980cee69e647fc53d72c0d0d260d3b
Sergey Alekseev <sergey@asoft.co>
2019-10-14 19:54:26 +0300

    add autocorrect before-after examples

--- autocorrect.rb
+++ autocorrect.rb
@@ -1,19 +1,45 @@
+# before `rubocop --safe-auto-correct autocorrect.rb`
 @variable = if @conditional
               1
             else
               2
             end
 
+# after `rubocop --safe-auto-correct autocorrect.rb`
+# @variable =
+#   if @conditional
+#     1
+#   else
+#     2
+#              end
+
+# before `rubocop --safe-auto-correct autocorrect.rb`
 @variable = begin
               1
             end
 
+# after `rubocop --safe-auto-correct autocorrect.rb`
+# @variable =
+#   begin
+#                1
+#              end
+
+# before `rubocop --safe-auto-correct autocorrect.rb`
 @variable = begin
               @something
             rescue StandardError
               false
             end
 
+# after `rubocop --safe-auto-correct autocorrect.rb`
+# @variable =
+#   begin
+#                @something
+#   rescue StandardError
+#     false
+#              end
+
+# before `rubocop --safe-auto-correct autocorrect.rb`
 @variable = begin
   if @conditional
     1
@@ -22,9 +48,29 @@
   end
 end
 
+# after `rubocop --safe-auto-correct autocorrect.rb`
+# @variable =
+#   begin
+#    if @conditional
+#      1
+#    else
+#      2
+#    end
+#  end
+
+# before `rubocop --safe-auto-correct autocorrect.rb`
 if @variable
   if @variable
     @variable = @conditional ?
       :a : :b
   end
 end
+
+# after `rubocop --safe-auto-correct autocorrect.rb`
+# if @variable
+#   if @variable
+#     @variable =
+#       @conditional ?
+#            :a : :b
+#   end
+# end
